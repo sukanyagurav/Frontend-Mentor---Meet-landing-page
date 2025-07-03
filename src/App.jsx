6dbe77bd6e7620d0a7ac850243acf1eebb26e976
sukanya gurav <sukanyagurav6@gmail.com>
2025-07-03 23:22:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './assets/logo.svg'
+import Banner from './components/Banner'
+function App() {
+
+
+  return (
+    <>
+    <header>
+      <img src={logo} alt='meet logo'/>
+    </header>
+    <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import leftImg from '../assets/desktop/image-hero-left.png'
+import rightImg from '../assets/desktop/image-hero-right.png'
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <img src={leftImg} alt="" />
+      <div className="banner__middle">
+        <h1>Group Chat for Everyone</h1>
+        <p>Meet makes it easy to connect with others face-to-face virtually and collaborate across any device.</p>
+        <div className="banner_btn-group">
+            <button className="btn green_btn">
+                Download v1.3
+            </button>
+            <button className="btn purple_btn">
+                what is it?
+            </button>
+        </div>
+      </div>
+      <img src={rightImg}/>
+    </div>
+  )
+}
+
+export default Banner
